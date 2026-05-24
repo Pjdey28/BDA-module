1faad2ff542742909bd6e746e582475add0bd2ca
Pranjal <sajalkumardey71@gmail.com>
2026-05-24 23:47:02 +0530

    Updated baseURL

--- frontend/src/api/axios.jsx
+++ frontend/src/api/axios.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api'
+  baseURL: 'https://bda-module-87qq.onrender.com/api'
 });
 
 API.interceptors.request.use((req) => {
